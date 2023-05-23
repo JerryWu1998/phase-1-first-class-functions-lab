f63c19d5d05fde1edb58c0167cb71a5e3096fe0f
Yu Wu <yw3748@columbia.edu>
2023-05-23 10:56:08 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(array) {
+  return array.slice(0, 2);
+}
+
+function returnLastTwoDrivers(array) {
+  return array.slice(-2);
+}
+
+const selectingDrivers = [(returnFirstTwoDrivers), (returnLastTwoDrivers)];
+
+function createFareMultiplier(num1) {
+  return function(num2) {
+    return num1 * num2;
+  };
+}
+
+function fareDoubler(num) {
+  return createFareMultiplier(2)(num);
+}
+
+function fareTripler(num) {
+  return createFareMultiplier(3)(num);
+}
+
+function selectDifferentDrivers(array, fn) {
+  return fn(array);
+}
